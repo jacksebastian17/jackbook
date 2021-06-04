e234cdc2db36f24e472ee9fe685ac7509fabfebc
jacksebastian17 <jacksebastian17@tamu.edu>
2021-06-04 02:11:06 -0500

    added user and profile forms

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm): # form to allow user to update username and email
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm): # form to allow user to update profile picture
+    class Meta:
+        model = Profile
+        field = ['image']
